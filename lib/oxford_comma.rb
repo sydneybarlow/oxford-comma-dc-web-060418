9d3cc9e38fefc8417044f6ee6c43f0a08372a929
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-01 04:34:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.join
+  elsif array.size == 2 
+    return array.join(" and ")
+  elsif array.size >= 3 
+    #grab all elements except for the last
+    last_element = array.pop
+    #join those elements in a string and save that as a local_variables
+    new_array = array.join(", ")
+    #add the and and the last element
+    together = new_array + ", and " + last_element
+    return together
+  end
 end
+
+
+# x = 1
+# if x > 2
+#   puts "x is greater than 2"
+# elsif x <= 2 and x!=0
+#   puts "x is 1"
+# else
+#   puts "I can't guess the number"
+# end
