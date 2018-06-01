1536caa3761323c58d18e5fb79027cf31647e31d
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-01 04:19:48 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2 
     return array.join(" and ")
   elsif array.size == 3
-    return array.last.join(" and ")
+    return (array.last).join(" and ")
   end
 end
 
